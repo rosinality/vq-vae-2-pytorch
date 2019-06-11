ccf582599a781f25d6bbfc6fd53f495d2b11a1de
Kim Seonghyeon <kim.seonghyeon@snu.ac.kr>
2019-06-11 03:01:53 +0000

    Added sampling script

---
+++ sample.py
@@ -0,0 +1,74 @@
+import argparse
+import os
+
+import torch
+from torchvision.utils import save_image
+from tqdm import tqdm
+
+from vqvae import VQVAE
+from pixelsnail import PixelSNAIL
+
+
+@torch.no_grad()
+def sample_model(model, device, batch, size, temperature, condition=None):
+    row = torch.zeros(batch, *size, dtype=torch.int64).to(device)
+
+    for i in tqdm(range(size[0])):
+        for j in range(size[1]):
+            out = model(row[:, : i + 1, :], condition=condition)
+            prob = torch.softmax(out[:, :, i, j] / temperature, 1)
+            sample = torch.multinomial(prob, 1).squeeze(-1)
+            row[:, i, j] = sample
+
+    return row
+
+
+def load_model(model, checkpoint, device):
+    model.load_state_dict(torch.load(os.path.join('checkpoint', checkpoint)))
+    model = model.to(device)
+    model.eval()
+
+    return model
+
+
+if __name__ == '__main__':
+    device = 'cuda'
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--batch', type=int, default=8)
+    parser.add_argument('--vqvae', type=str)
+    parser.add_argument('--top', type=str)
+    parser.add_argument('--bottom', type=str)
+    parser.add_argument('--temp', type=float, default=1.0)
+    parser.add_argument('filename', type=str)
+
+    args = parser.parse_args()
+
+    model_vqvae = VQVAE()
+    model_top = PixelSNAIL([32, 32], 512, 256, 5, 4, 4, 256)
+    model_bottom = PixelSNAIL(
+        [64, 64],
+        512,
+        256,
+        5,
+        3,
+        4,
+        256,
+        attention=False,
+        n_cond_res_block=3,
+        cond_res_channel=256,
+    )
+
+    model_vqvae = load_model(model_vqvae, args.vqvae, device)
+    model_top = load_model(model_top, args.top, device)
+    model_bottom = load_model(model_bottom, args.bottom, device)
+
+    top_sample = sample_model(model_top, device, args.batch, [32, 32], args.temp)
+    bottom_sample = sample_model(
+        model_bottom, device, args.batch, [64, 64], args.temp, condition=top_sample
+    )
+
+    decoded_sample = model_vqvae.decode_code(top_sample, bottom_sample)
+    decoded_sample = decoded_sample.clamp(-1, 1)
+
+    save_image(decoded_sample, args.filename, normalize=True, range=(-1, 1))
